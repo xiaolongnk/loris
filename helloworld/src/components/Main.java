8ae44b89705b1da0c385ec7f0d943302b5e4529f
xiaolongnk <xiaolongnk@126.com>
2014-05-24 14:08:03 +0800

    This is my new version, just test ArrayList.

--- helloworld/src/components/Main.java
+++ helloworld/src/components/Main.java
@@ -1,9 +1,22 @@
 package components;
 
+import java.util.ArrayList;
+
+
 
 public class Main {
+
+	public Main(){
+		ArrayList t = new ArrayList();
+		t.add("hello");
+		t.add("hello1");
+		for(int i=0; i<t.size(); i++) {
+			System.out.println(t.get(i));
+		}
+	}
 	
 	public static void main(String args[]){
-		System.out.println("Hello world!");
+		new Main();
 	}
+	
 }
